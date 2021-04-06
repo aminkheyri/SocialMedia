eb700d0548564454223b78eeeff17a3f00d60f19
amin <kheyrriamin@yahoo.com>
2021-04-06 09:56:24 +0430

    User Dashboard And Dashboard Posts

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
 
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField(max_length=700)
