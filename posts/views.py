d86b5765c1a9d30ec0a8f4027489364bf8fa484e
amin <kheyrriamin@yahoo.com>
2021-04-07 22:16:26 +0430

    Edit

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
-from .forms import AddPostForm
+from .forms import AddPostForm, EditPostForm
 from django.contrib import messages
 from django.utils.text import slugify
 from django.contrib.auth.decorators import login_required
@@ -41,5 +41,24 @@ def post_delete(request, user_id, post_id):
         Post.objects.filter(pk=post_id).delete()
         messages.success(request, 'Your Post Deleted Successfully', 'success')
         return redirect('account:dashboard', user_id)
+    else:
+        return redirect('posts:all_posts')
+
+
+@login_required
+def post_edit(request, user_id, post_id):
+    if request.user.id == user_id:
+        post = get_object_or_404(Post, pk=post_id)
+        if request.method == "POST":
+            form = EditPostForm(request.POST, instance=post)
+            if form.is_valid():
+                edit_post = form.save(commit=False)
+                edit_post.slug = slugify(form.cleaned_data['body'][:30])
+                edit_post.save()
+                messages.success(request, 'Your Post Edited', 'success')
+                return redirect('account:dashboard', user_id)
+        else:
+            form = EditPostForm(instance=post)
+        return render(request, 'posts/edit_post.html', {'form': form})
     else:
         return redirect('posts:all_posts')
