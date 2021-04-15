4c91b0ec06729e73c4a618143d67d98cf5f8a298
amin <kheyrriamin@yahoo.com>
2021-04-16 02:58:52 +0430

    ajax following(final)

--- account/views.py
+++ account/views.py
@@ -8,8 +8,7 @@ from django.contrib.auth.decorators import login_required
 from random import randint
 from kavenegar import *
 from .models import Profile, Relation
-from django import forms
-
+from django.http import JsonResponse
 
 def user_login(request):
     next = request.GET.get('next')
@@ -122,9 +121,27 @@ def verify(request):
     return render(request, 'account/verify.html', {'form': form})
 
 
+@login_required
 def follow(request):
-    pass
+    if request.method == "POST":
+        user_id = request.POST['user_id']
+        following = get_object_or_404(User, pk=user_id)
+        check_relation = Relation.objects.filter(from_user=request.user, to_user=following)
+        if check_relation.exists():
+            return JsonResponse({'status': 'exists'})
+        else:
+            Relation(from_user=request.user, to_user=following).save()
+            return JsonResponse({'status': 'ok'})
 
 
+@login_required
 def unfollow(request):
-    pass
+    if request.method == "POST":
+        user_id = request.POST['user_id']
+        following = get_object_or_404(User, pk=user_id)
+        check_relation = Relation.objects.filter(from_user=request.user, to_user=following)
+        if check_relation.exists():
+            check_relation.delete()
+            return JsonResponse({'status': 'ok'})
+        else:
+            return JsonResponse({'status': 'notexists'})
